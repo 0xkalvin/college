18589ae3ab678836e252b52af4237d59a032bbe7
kalvin <kalvinvini1998@gmail.com>
2019-04-01 10:11:37 -0300

    some mips basics

--- assembly/basics.asm
+++ assembly/basics.asm
@@ -17,8 +17,21 @@ else                    add $s2, $s0, $s1
 
 c = 0           --->    # i = $s0; c = $s1
 for(i = 0; i < 10; i++) for: beq $s0, 10, done
-    c++;                     add $s1, $s1, 1
-                             add $s0, $s0, 1
+    c+=2;                     add $s1, $s1, 2
+                              add $s0, $s0, 1
                              j for
                         done: la $s2, $s1
 
+
+
+li $s0, 3           int a = 3
+li $s1, 4           int b = 4
+li $s2, 10          int m = 10
+
+lw $t0, 0($s0)      
+sw $t0, 0($s2)      m = a
+
+slt $t1, $s1, $s2
+beq $t1, 1, target
+
+target: lw $t3, 0($s2)
