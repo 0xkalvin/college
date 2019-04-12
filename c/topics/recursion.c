8934c97fe4bf47f8ff8c6c6fef606f85a733efca
kalvin <kalvinvini1998@gmail.com>
2019-04-12 19:27:28 -0300

    recursion

--- c/topics/recursion.c
+++ c/topics/recursion.c
@@ -6,6 +6,11 @@ int somaIntervalo(int start,int end){
     else return end + somaIntervalo(start, end-1);
 }
 
+int arraySoma(int array[], int size){
+    if(size == 0) return 0;
+    else return array[size-1] + arraySoma(array, size-1);
+}
+
 int fibonacci(int n){
     if(n<=1) return n;
     else return fibonacci(n-1) + fibonacci(n-2);
@@ -24,12 +29,31 @@ void printArray(int array[], int size, int i){
     }
 }
 
+int binarySearch(int array[], int n, int start, int end){
+    if(start > end) return -1;
+    else{
+        int mid = (start + end)/2;
+        if(array[mid] == n) return mid;
+        else if(array[mid] > n) return binarySearch(array, n, start, mid-1);
+        else return binarySearch(array, n, mid+1, end);
+    }
+}
+
+
+int powerOf(int x, int y){
+    if(y == 0) return 1;
+    else return x*powerOf(x, y-1);
+}
 
 
 int main(){
 
     printf(" %d", somaIntervalo(0,4));
     printf(" %d \n", fatorial(4));
+
+    int array[] = {1,2,3};
+
+    printf(" %d ", arraySoma(array, 3));
 
 
     return 0;
