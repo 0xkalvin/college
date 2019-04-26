6932186cf190bd42a3eb9186bc51504b29299300
kalvin <kalvinvini1998@gmail.com>
2019-04-26 20:51:23 -0300

    doubly linked list

--- c++/exercises/007linkedlist/dll.cpp
+++ c++/exercises/007linkedlist/dll.cpp
@@ -10,3 +10,23 @@ template <typename T>
 Dll<T>::~Dll(){
     delete this->head;
 }
+
+template <typename T>
+bool Dll<T>::isEmpty() const{
+    return this->head == NULL;
+}
+
+template <typename T>
+const T &Dll<T>::getFirst() const {
+    return this->head->elem;
+}
+
+template <typename T>
+const T &Dll<T>::getLast() const {
+    return this->tail->elem;
+}
+
+template <typename T>
+Node<T> *Dll<T>::getHead() const {
+    return this->head;
+}
