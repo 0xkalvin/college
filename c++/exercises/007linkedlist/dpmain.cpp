ff418e89a59253a48ca0a93681cf4b0dcf02cc96
kalvin <kalvinvini1998@gmail.com>
2019-05-02 14:49:40 -0300

    main

--- c++/exercises/007linkedlist/dpmain.cpp
+++ c++/exercises/007linkedlist/dpmain.cpp
@@ -1,15 +1,19 @@
 #include "dpolynomial.h"
 
+using namespace std;
 
 int main(){
 
     DPolynomial p1;
 
-    p1.insertLast(Monomial(2,1));
-    p1.insertLast(Monomial(-3,2));
-    p1.insertLast(Monomial(4,3));
+    p1.insertLast(Monomial(1,1));
+    p1.insertLast(Monomial(2,3));
+    p1.insertLast(Monomial(3,3));
 
+    cout << "p(x) is ";
     p1.print();
+
+    cout << "p(1) is " << p1.resolve(1) <<endl;
 
     
     return 0;
