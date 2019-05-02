cd06b8071961da867d15374cdd91f2e97d6657db
kalvin <kalvinvini1998@gmail.com>
2019-05-02 14:49:18 -0300

    resolve method

--- c++/exercises/007linkedlist/dpolynomial.cpp
+++ c++/exercises/007linkedlist/dpolynomial.cpp
@@ -15,3 +15,20 @@ void DPolynomial::print(){
     }
     std::cout <<std::endl;
 }
+
+
+double DPolynomial::resolve(int k){
+    DNode<Monomial> *auxStart = this->getHead();
+    DNode<Monomial> *auxEnd = this->getTail();
+    double sum = 0;
+    while(auxStart != NULL && auxEnd != NULL){
+        if(auxStart == auxEnd)    return sum + auxStart->elem.getCoefficient()*pow(k, auxStart->elem.getExponent());
+        else if(auxStart->prev == auxEnd) return sum;
+        else{
+            sum += auxStart->elem.getCoefficient()*pow(k, auxStart->elem.getExponent()) + auxEnd->elem.getCoefficient()*pow(k, auxEnd->elem.getExponent());
+            auxStart = auxStart->next;
+            auxEnd = auxEnd->prev;
+        }
+    }
+    return sum;
+}
