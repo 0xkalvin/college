d6550f2a8d6f301beb05c0f1f63438c622b24cd6
kalvin <kalvinvini1998@gmail.com>
2019-05-02 10:00:09 -0300

    polynomial resolve method

--- c++/exercises/007linkedlist/pmain.cpp
+++ c++/exercises/007linkedlist/pmain.cpp
@@ -1,17 +1,22 @@
 #include "polynomial.h"
 
-
+using namespace std;
 
 int main(){
 
     Polynomial p1;
 
 
-    p1.insertFirst(Monomial(1.2, 2));
-    p1.insertFirst(Monomial(6, 3));
-    p1.insertFirst(Monomial(2.5, 1));
+    p1.insertFirst(Monomial(2, 2));
+    p1.insertFirst(Monomial(2, 3));
+    p1.insertFirst(Monomial(2, 1));
 
+    cout << "p(x) is ";
     p1.print();
+
+
+    cout << "p(0) is " << p1.resolve(0) <<endl;
+    cout << "p(1) is " << p1.resolve(1) <<endl;
 
 
     return 0;
--- c++/exercises/007linkedlist/polynomial.cpp
+++ c++/exercises/007linkedlist/polynomial.cpp
@@ -1,5 +1,5 @@
 #include "polynomial.h"
-
+#include <cmath>
 
 Polynomial::Polynomial() : LinkedList<Monomial>(){}
 
@@ -8,8 +8,20 @@ Polynomial::~Polynomial(){}
 void Polynomial::print(){
     Node<Monomial> *aux = this->getHead();
     while(aux != NULL){
-        std::cout <<" "<< aux->elem.getCoefficient() << "x^" << aux->elem.getExponent();
+        if(aux->elem.getCoefficient() > 0)  std::cout <<" + "<< aux->elem.getCoefficient() << "x^" << aux->elem.getExponent();
+        else    std::cout <<" "<< aux->elem.getCoefficient() << "x^" << aux->elem.getExponent();
         aux = aux->next;
     }
     std::cout <<std::endl;
+}
+
+
+double Polynomial::resolve(int k){
+    double sum = 0;
+    Node<Monomial> *aux = this->getHead();
+    while(aux != NULL){
+        sum += aux->elem.getCoefficient()*pow(k, aux->elem.getExponent());
+        aux = aux->next;
+    }
+    return sum;
 }
