d29b0551a86f01f371e233b1c022249f35008091
kalvin <kalvinvini1998@gmail.com>
2019-05-05 18:16:12 -0300

    merge

--- c/exercises/001merge.c
+++ c/exercises/001merge.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// a1 = 1,5,6
-// a2 =  2, 123, 1000
-// a3 = 1, 2, 5, 6, 123, 1000
-
-void printArray(int *array, int tam)
-{
-    printf(" [");
-    for (int i = 0; i < tam; i++)   printf(" %2d", array[i]);
-    printf(" ]");
-    printf("\n\n");
-}
+void printArray(int *array, int tam);
 
 int *intercala(int vetor1[], int vetor2[], int tam1, int tam2)
 {
@@ -37,15 +27,59 @@ int *intercala(int vetor1[], int vetor2[], int tam1, int tam2)
     return resultado;
 }
 
+
+// n (arraySize) = end - start + 1
+// start = 0
+// end = n - 1
+// mid = (start + end) / 2
+void merge(int array[], int start, int mid, int end){
+    // set sizes for both arrays
+    int n1 = mid - start + 1; 
+    int n2 = end - mid;
+    // create left and right array;
+    int left[n1];
+    int right[n2];
+    
+    int i, j, k;
+    // fill both arrays 
+    for(i = 0; i < n1; i++) left[i] = array[start + i];
+    for(j = 0; j < n2; j++) right[j] = array[mid + 1 + j];
+
+    i = 0;
+    j = 0; 
+    for(k = start; k < end; k++){
+        if(left[i] <= right[j]){
+            array[k] = left[i];
+            i++;
+        }
+        else{
+            array[k] = right[j];
+            j++;
+        }
+    }
+}
+
 int main()
 {
 
-    int vetor1[] = {0,10, 20, 30, 40, 50};
-    int vetor2[] = {11, 21, 31, 41, 51};
+    int array[] = {10, 20, 30, 40, 50, 11, 21, 31, 41, 51};
+    int start = 0;
+    int end = sizeof(array)/sizeof(array[0]) - 1;
+    int mid = (start + end) / 2;
 
-    int *vetorIntercalado;
-    vetorIntercalado = intercala(vetor1, vetor2, 5, 5);
-    printArray(vetorIntercalado, 10);
+    printArray(array, 10);
+
+    merge(array, start, mid, end);
+
+    printArray(array, 10);
 
     return 0;
+}
+
+void printArray(int *array, int tam)
+{
+    printf(" [");
+    for (int i = 0; i < tam; i++)   printf(" %2d", array[i]);
+    printf(" ]");
+    printf("\n\n");
 }
