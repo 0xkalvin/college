d1322b59bd05fb70c40b7abfb7877a1f57d6dad5
kalvin <kalvinvini1998@gmail.com>
2019-09-02 20:08:32 -0300

    hash table basics

---
+++ python/hashtable.py
@@ -0,0 +1,30 @@
+
+SIZE=97
+
+class HashTable:
+    def __init__(self):
+        self.array = [ None for i in range(SIZE)]
+
+    def hash(self, key):
+        return len(key) % (SIZE//2)
+
+    def insert(self, key, value):
+        index = self.hash(key)
+        self.array[index] = value
+
+    def search(self, key):
+        index = self.hash(key)
+        return self.array[index] if self.array[index] is not None else  "Chave nao existe"
+
+
+
+
+
+
+if __name__ == "__main__":
+    my_table = HashTable()
+
+    my_table.insert("Roger", "Verdinho")
+
+    print(my_table.search("Roger"))
+    print(my_table.search("Kaique"))
