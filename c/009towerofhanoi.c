7c52f76b012db423968a7348581eb887e950b848
kalvin <kalvinvini1998@gmail.com>
2019-09-13 13:37:03 -0300

    tower of hanoi recursive

--- c/009towerofhanoi.c
+++ c/009towerofhanoi.c
@@ -2,13 +2,30 @@
 #include <stdlib.h>
 
 
-void towerOfHanoi(int disks, char source, char aux, char destination){
-    
+/*
+    (2 ^ n) - 1
+
+    n = 3 -> 8 - 1 = 7
+    n = 4 -> 16 - 1 = 15
+    n = 5 -> 32 - 1 = 31
+*/
+void tower(int n, char source, char auxilary, char destination){
+    if (n == 1){
+        printf("\t\tMove disk %d source %c destination %c\n", n, source, destination);
+        return;
+    }
+    else{
+        tower(n - 1, source, destination, auxilary);
+        printf("\t\tMove disk %d source %c destination %c\n", n, source, destination);
+        tower(n - 1, auxilary, source, destination);
+    }
 }
 
 int main(){
 
+    int n = 3;
+    printf("\t\tTOWER OF HANOI \n");
+    tower(n, 'A', 'B', 'C');
 
     return 0;
 }
-
