44e517a46db6b09f46a7cb688215789ae6555854
kalvin <kalvin.vasconcellos@gmail.com>
2020-02-19 14:43:04 -0300

    add exercise 4

--- programming/c/parallelComputing/001intro.c
+++ programming/c/parallelComputing/001intro.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+    Exercises:
+    http://www.inf.pucrs.br/~pinho/PRGSWB/Exercicios/Introducao/Introducao.html
+*/
 
 void exercise3(int n){
 
@@ -24,12 +28,38 @@ void exercise3(int n){
         printf("\n");
         counter++;
     }
-
 }
+
+void exercise4(){
+
+    char name1[100], name2[100];
+
+    printf("\nInsert the first name\n");
+    scanf("%s", name1);
+
+    printf("\nInsert the second name\n");
+    scanf("%s", name2);
+
+    int counter1 = 0, counter2 = 0, i =  0;
+    
+    while(counter1 == counter2 && i < 100){
+        counter1 += name1[i];
+        counter2 += name2[i];
+        i++;
+    }
+    
+    printf("\nNames in alphabetical order: \n");
+    if(counter1 < counter2){
+        printf("%s \n%s \n", name1, name2);
+    } else {
+        printf("%s \n%s \n", name2, name1);
+    }
+} 
+
 
 int main(void){
 
-    exercise3(9);
+    exercise4();
 
     return 0;
 }
