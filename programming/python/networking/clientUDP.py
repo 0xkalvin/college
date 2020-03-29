2e129fff13876b73cdff93e59123db2237a4dc41
Kalvin <kalvin.vasconcellos@gmail.com>
2020-03-29 11:35:46 -0300

    udp chat: server-client implementatiom

---
+++ programming/python/networking/clientUDP.py
@@ -0,0 +1,28 @@
+import socket  
+  
+UDP_IP = "192.168.15.14"  
+UDP_PORT = 5005          
+ENCODING = 'UTF-8'
+BUFFER_SIZE = 1024
+
+# Criação de socket UDP
+# SOCK_DGRAM, indica que será UDP.
+client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+while True:
+    messageToServer  = input("Enter your message for the server: ")
+    client.sendto(messageToServer.encode(ENCODING), (UDP_IP, UDP_PORT))
+
+    if messageToServer == "quit":
+        client.close()
+        break
+
+    data, addr = client.recvfrom(BUFFER_SIZE)
+    messageFromServer = data.decode(ENCODING)
+    print("New message from server: \n", messageFromServer)
+
+    if messageFromServer == "quit":
+        client.close()
+        break
+    
+
---
+++ programming/python/networking/serverUDP.py
@@ -0,0 +1,33 @@
+import socket
+
+UDP_IP = "192.168.15.14" 
+UDP_PORT = 5005
+ENCODING = 'UTF-8'
+BUFFER_SIZE = 1024
+
+# Criação de socket UDP
+# Argumentos, AF_INET que declara a família do protocolo; se fosse um envio via Bluetooth usariamos AF_BLUETOOTH
+# SOCK_DGRAM, indica que será UDP.
+server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
+
+# IP e porta que o servidor deve aguardar a conexão
+server.bind((UDP_IP, UDP_PORT)) 
+print("Server listening on port %d" % (UDP_PORT)) 
+ 
+while True:
+    data, addr = server.recvfrom(BUFFER_SIZE)
+    messageFromClient = data.decode(ENCODING)
+
+    if messageFromClient == "quit":
+        server.close()
+        break
+    
+    print("New message from client: ", addr)
+    print (messageFromClient)
+
+    messageToClient = input("Enter your message for the client: ")    
+    server.sendto(messageToClient.encode(ENCODING), (addr[0], addr[1]))
+
+    if messageToClient == "quit":
+        server.close()
+        break
