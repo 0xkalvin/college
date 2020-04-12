2f7fc9f6e741c8d12b82013e521a1d26a90bfec6
kalvin <kalvin.vasconcellos@gmail.com>
2020-04-12 13:53:47 -0300

    automata: now it works huh

--- programming/c/automata/main.c
+++ programming/c/automata/main.c
@@ -1,215 +1,152 @@
-/*Kalvin 31894666
-  Roger  31849571
-  Wendel 31811957 */
-
 #include <stdio.h>
-#define MAX_INPUT_LEN 200
 
-void scanner(char word[MAX_INPUT_LEN])
-{
-    FILE * fp;
-    int wordSize = MAX_INPUT_LEN;
-    int notAccepted = 0;
+#define MAX_LEN_WORDS 100
+
+
+
+int getWordsLength(char words[MAX_LEN_WORDS]){
+    int counter = 0;
+    
+    while(words[counter] != '\0') {
+        counter++;
+    }
+    
+    return counter;
+}
+
+
+void processWords(char words[MAX_LEN_WORDS]){
+
+    FILE *file;
+    file = fopen("result.txt", "w");
+
     int finalState = 0;
     int i = 0;
-    fp = fopen("result.txt", "w");
-    char c;
-   
-    while (i < wordSize)
-    {
-    goback:
+    int size = getWordsLength(words);
+    char c = words[0];
 
-        c = word[i];
 
-    q0:
-        c = word[i];
-        finalState = 0;
+    while(i < size){
+        back:            
+            
+            c = words[i];
+           
+            q0:
+                c = words[i];
+                finalState = 0;
+                if(c >= 48 && c <= 57){
+                    i++;
+                    goto q6;
+                } else if(c == 43 || c == 45){
+                    i++;
+                    goto q1;
+                } else if(c == 32){
+                    i++;
+                    goto q0;
+                } else  {
+                    goto q4;
+                }
 
-        if (c >= 48 && c <= 57)
-        {
-            i++;
-            goto q6;
-        }
-        else if (c == 43 || c == 45)
-        {
-            i++;
-            goto q1;
-        }
-        else if (c == 32)
-        {
-            i++;
-            finalState = 0;
-            goto whitespace;
-        }
-        else if (c == '\0')
-        {
-            finalState = 0;
-            break;
-        }
-        else
-        {
-            goto q4;
-        }
+            q1:
+                c = words[i];
+                finalState = 1;
+                if(c >= 48 && c <= 57){
+                    i++;
+                    goto q5;
+                } else  {
+                    goto q4;
+                }
 
-    q1:
-        c = word[i];
+            q2:
+                c = words[i];
+                finalState = 2;
+                if(c >= 48 && c <= 57){
+                    i++;
+                    goto q2;
+                } else if(c == 32 || c == '\0'){
+                    i++;
+                    goto printWord;
+                } else  {
+                    goto q4;
+                }
+            q3:
+                c = words[i];
+                finalState = 3;
+                if(c >= 48 && c <= 57){
+                    i++;
+                    goto q2;
+                } else  {
+                    goto q4;
+                }
+            q4:
+                c = words[i];
+                i++;
+                finalState = 4;
+                goto printWord;
+                
+            q5:
+                c = words[i];
+                finalState = 5;
+                if(c >= 48 && c <= 57){
+                    i++;
+                    goto q5;
+                } else if(c == 44 || c == 46){
+                    i++;
+                    goto q3;
+                } else if(c == 32 || c == '\0'){
+                    i++;
+                    goto printWord;
+                } else  {
+                    goto q4;
+                }
+            
+            q6:
+                c = words[i];
+                finalState = 6;
+                if(c >= 48 && c <= 57){
+                    i++;
+                    goto q6;
+                } else if(c == 44 || c == 46){
+                    i++;
+                    goto q3;
+                } else if(c == 32 || c == '\0'){
+                    i++;
+                    goto printWord;
+                } else  {
+                    goto q4;
+                }
 
-        if (c >= 48 && c <= 57)
-        {
-            i++;
-            goto q5;
-        }
-        else if (c == '\0')
-        {
-            finalState = 1;
-            break;
-        }
-        else if (c == 32)
-        {
-            i++;
-            finalState = 1;
+            
+            printWord:
 
-            goto whitespace;
-        }
-        else
-            goto q4;
+                if(finalState == 4){
+                    printf("ERROR\n");
+                    fprintf(file, "ERROR\n");
+                    
+                    while(c != 32 && c != '\0'){
+                            i++;
+                            c = words[i];
+                    }
 
-    q2:
-        c = word[i];
-
-        if (c >= 48 && c <= 57)
-        {
-            i++;
-            goto q2;
-        }
-        else if (c == '\0')
-        {
-            finalState = 2;
-            break;
-        }
-                else if (c == 32)
-        {
-            i++;
-            finalState = 2;
-
-            goto whitespace;
-        }
-        else
-            goto q4;
-    q3:
-        c = word[i];
-
-        if (c >= 48 && c <= 57)
-        {
-            i++;
-            goto q2;
-        }
-        else if (c == '\0')
-        {
-            finalState = 3;
-            break;
-        }
-        else if (c == 32)
-        {
-            i++;
-            finalState = 3;
-
-            goto whitespace;
-        }
-
-        else
-            goto q4;
-
-    q4:
-        i++;
-        notAccepted = 1;
-        finalState = 4;
-        goto whitespace;
-
-    q5:
-        c = word[i];
-
-        if (c >= 48 && c <= 57)
-        {
-            i++;
-            goto q5;
-        }
-        else if (c == 44 || c == 46)
-        {
-            i++;
-            goto q3;
-        }
-        else if (c == '\0')
-        {
-            finalState = 5;
-            break;
-        }
-        else if (c == 32)
-        {
-            i++;
-            finalState = 5;
-
-            goto whitespace;
-        }
-        else
-            goto q4;
-    q6:
-        c = word[i];
-
-        if (c >= 48 && c <= 57)
-        {
-            i++;
-            goto q6;
-        }
-        else if (c == 44 || c == 46)
-        {
-            i++;
-            goto q3;
-        }
-        else if (c == 32)
-        {
-            i++;
-            finalState = 6;
-            goto whitespace;
-        }
-        else if (c == '\0')
-        {
-            finalState = 6;
-            break;
-        }
-
-        else
-            goto q4;
-
-    
-    whitespace:
-        if (notAccepted)
-        {
-            printf("Error\n");
-            fprintf(fp, "ERRO\n");
-        }
-
-        if (finalState == 6 || finalState == 5)
-        {
-            printf("INTEIRO\n");
-            fprintf(fp, "<NUMERO INTEIRO>\n");
-        }
-        else if (finalState == 2)
-        {
-            printf("PONTO FLUTUANTE\n");
-            fprintf(fp, "<NUMERO COM PONTO FLUTUANTE>\n");
-        }
-        fclose(fp);
-        if(c != '\0')   goto goback;
+                } else if(finalState == 5 || finalState == 6){
+                    printf("INTEIRO\n");
+                    fprintf(file,"INTEIRO\n");
+                } else if(finalState == 2){
+                    printf("PONTO FLUTUANTE\n");       
+                    fprintf(file,"PONTO FLUTUANTE\n");
+                }  
+                
+                if(c == '\0'){ 
+                    break;
+                }
+                goto back;
     }
 }
 
-int main(void)
-{
+int main(){
 
-    char word[MAX_INPUT_LEN] = "-21 +45.67 0.123 .456 42 23. xx";
-
-    scanner(word);
+    char words[MAX_LEN_WORDS] = "-21 +45.67 0.123 .456 42 23. xx";
+    processWords(words);
 
     return 0;
 }
